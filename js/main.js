eff6a2e4b072b0f496954f25f10f4a7351170355
anchen <li.anchen.au@gmail.com>
2015-09-17 22:52:22 +1000

    add pages

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var peer = new Peer({key: '5mvo0w847divbo6r'});
+var selfPeerId;
+peer.on('open', function (id) {
+    selfPeerId = id;
+    $('#localPeerId').text('your peer id is '+id);
+});
+peer.on('connection', onConnect);
+
+//connect to remote
+$('#connectRemote').click(function(){
+    var remoteConn = peer.connect($('#remoteId').val());
+    remoteConnection(remoteConn);
+});
+
+//recieved remote connection
+function onConnect(conn) {
+    remoteConnection(conn);
+}
+
+function remoteConnection(conn) {
+    conn.on('open', function () {
+        // Receive messages
+        conn.on('data', function (data) {
+            new google.maps.Marker({
+                position: data,
+                map: window.map,
+                title:  new Date()
+            });
+        });
+        // Send messages
+        navigator.geolocation.watchPosition(conn.send)
+
+    });
+}
